a4d4b9ee946c4183ddb2067edd854f51821cf8b6
Sulla2012 <jackorlowskischerer@gmail.com>
2020-04-06 15:37:11 -0400

    Changes to be committed: 	modified:   guassian_beam_gen.py 	Added file writing functionality

--- guassian_beam_gen.py
+++ guassian_beam_gen.py
@@ -18,8 +18,18 @@ def make_gaus(size = 128, res = 0.1, center = None, data_spacing = 0.01):
     #Generate matrix
     gaus = np.exp(-4*np.log(2) * ((x-x0)**2 + (y-y0)**2) / res**2)
       
+    return gaus, x0, y0[0] 
 
-    return gaus 
+def make_file(filename, size = 128, res = 0.1, center = None, data_spacing = 0.01):
+    gaus, x0, y0 = make_gaus(size = size, res = res, center = center, data_spacing = data_spacing)
+    with open(str(filename)+'.txt', 'w') as f:
+        f.write('Pure Guassian beam\nData spacing is: ' + str(data_spacing) + ' degrees \nData area is: ' + str(size*data_spacing) + ' degrees square \nCenter point is: ' + str(x0) + ', ' + str(y0) + '\nValues are relative intensity\n\n')
 
-def make_file(filename, size = 128, res = 0.1, center = None, data_space = 0.01):
+        for i in range(len(gaus[0])):
+            for j in range(len(gaus[0])):
+                f.write(str(gaus[i][j]) + '\t')
+            f.write('\n')
+         
+    return
 
+make_file('test')
