11a315daf5ebea052c63c099c5ed9a62500ba415
Sulla2012 <jackorlowskischerer@gmail.com>
2020-04-06 15:53:35 -0400

    Changes to be committed: 	modified:   guassian_beam_gen.py 	Added 280GHz sim

--- guassian_beam_gen.py
+++ guassian_beam_gen.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def make_gaus(size = 128, res = 0.1, center = None, data_spacing = 0.01):
+def make_gaus(filename, size = 128, res = 0.1, center = None, data_spacing = 0.01):
 
     #Make an x and y axis
     x = np.arange(0, size*data_spacing, data_spacing, float)
@@ -17,11 +17,13 @@ def make_gaus(size = 128, res = 0.1, center = None, data_spacing = 0.01):
         
     #Generate matrix
     gaus = np.exp(-4*np.log(2) * ((x-x0)**2 + (y-y0)**2) / res**2)
-      
+    plt.imshow(gaus)
+    plt.savefig(str(filename)+'.pdf')
+    plt.show()
     return gaus, x0, y0[0] 
 
 def make_file(filename, size = 128, res = 0.1, center = None, data_spacing = 0.01):
-    gaus, x0, y0 = make_gaus(size = size, res = res, center = center, data_spacing = data_spacing)
+    gaus, x0, y0 = make_gaus(filename = filename, size = size, res = res, center = center, data_spacing = data_spacing)
     with open(str(filename)+'.txt', 'w') as f:
         f.write('Pure Guassian beam\nData spacing is: ' + str(data_spacing) + ' degrees \nData area is: ' + str(size*data_spacing) + ' degrees square \nCenter point is: ' + str(x0) + ', ' + str(y0) + '\nValues are relative intensity\n\n')
 
@@ -32,4 +34,4 @@ def make_file(filename, size = 128, res = 0.1, center = None, data_spacing = 0.0
          
     return
 
-make_file('test')
+make_file('LATR_280_gaus', res = 0.015)
