54ec40311d35493b0753eb1a158e279fb8d96c38
Sulla2012 <jackorlowskischerer@gmail.com>
2020-06-13 14:21:56 -0400

    removing excess files

--- gaussian_beam_gen.py
+++ gaussian_beam_gen.py
@@ -93,7 +93,7 @@ def make_file(filename, size = 128, res = 0.1, center = None, data_spacing = 0.0
 def _gaussian(M, *args):
     x, y = M
     arr = np.zeros(x.shape)
-    arr += gaussian(*args[0:5])(x,y)
+    arr += gaussian(*args[0:6])(x,y)
     return arr
 
 
@@ -102,12 +102,12 @@ def _gaussian(M, *args):
 data, meta = load_beam_txt('LAT_beam_1100um_center_tube1.TXT')
 
 #Following two functions courtousy of Scipy cookbook
-def gaussian(scale, center_x, center_y, width_x, width_y):
+def gaussian(scale,y_offset, center_x, center_y, width_x, width_y):
     """Returns a gaussian function with the given parameters"""
     width_x = float(width_x)
     width_y = float(width_y)
-    return lambda x,y: scale*np.exp(
-                -(((center_x-x)/width_x)**2+((center_y-y)/width_y)**2)/2)
+    return lambda x,y: y_offset + scale*np.exp(
+                -(((x-center_x)/width_x)**2+((y-center_y)/width_y)**2)/2)
 
 def fitgaussian(data):
     """Returns (height, x, y, width_x, width_y)
@@ -202,41 +202,84 @@ print("Starting SAT Beams")
 sat_beam = pickle.load(open('fitting_det0000_90_test2_wide.pkl', 'rb'))
 
 plt.imshow(np.log(sat_beam['data']), extent = [-32.12481, 17.87519,  16.29601, -33.70399])
+plt.savefig("SAT_beam.pdf")
 plt.show()
 
 X, Y = sat_beam['mesh'][0], sat_beam['mesh'][1]
 xdata = np.vstack((X.ravel(), Y.ravel()))
 
-p0 = [-1,-7,-8,.5,.5]
+p0 = [.5, -1,-7,-8,.5,.5]
+
+
+datamax = np.argmax(sat_beam['data'])
+print(sat_beam['data'].ravel()[datamax])
+print(xdata.ravel()[datamax])
+print("Y max: {}".format(Y.ravel()[datamax]))
+print("X max: {}".format(X.ravel()[datamax]))
+
+i,j = np.unravel_index(sat_beam['data'].argmax(), sat_beam['data'].shape)
+print("Peak: {}".format(sat_beam['data'][i,j]))
+#print(sat_beam['mesh'][0][i,j])
+#print((-32.12481 + 17.87519)/1001*i)
+#print((-33.70399 + 16.29601)/1001*j)
+print("Pixel size: {}".format((-33.70399 + 16.29601)/1001))
 
 popt, pcov = curve_fit(_gaussian, xdata, sat_beam['data'].ravel(),p0)
 print(popt)
 
+#Fit Plot
 x = np.linspace(-32.12481, 17.87519, 1001)
 y = np.linspace(-33.70399, 16.29601, 1001)
 y = y[:, np.newaxis]
-gaus2 = gaussian(*popt[0:5])(x,y)
+gaus2 = gaussian(*popt[0:6])(x,y)
 print(gaus2.shape)
 plt.imshow(np.log(gaus2), extent = [-32.12481, 17.87519,  16.29601, -33.70399])
 plt.savefig('SAT_90_0000_wide_fit_gaus.pdf')
 plt.show()
 
+#Fit and Data Slice
+print(sat_beam['data'].shape)
+yslice_data = sat_beam['data'][i]
+yslice_fit = gaus2[i]
+plt.plot(x,np.log(yslice_data), label = "data")
+plt.plot(x,np.log(yslice_fit), label = "fit")
+plt.legend()
+plt.savefig("slice_fit_data.pdf")
+plt.show()
+
+#Data Slice
+plt.plot(x,np.log(yslice_data), label = "data")
+#plt.plot(x,np.log(yslice_fit), label = "fit")
+#plt.legend()
+plt.savefig("slice_data.pdf")
+plt.show()
+
+#2D Residual Plots
 res2 = sat_beam['data']-gaus2
 plt.imshow(np.log(res2), extent = [-32.12481, 17.87519,  16.29601, -33.70399])
 plt.savefig('SAT_90_0000_wide_residual.pdf')
 plt.show()
 
+#Midpoint Residual Plots
 midpoint = int(len(res2[1])/2)
 
 yslice = res2[midpoint]
-xmin, xmax = -0.64, 0.64
-xslice = np.linspace(xmin, xmax, len(y))
+#xmin, xmax = -0.64, 0.64
+#xslice = np.linspace(xmin, xmax, len(y))
 
-plt.plot(xslice, np.log(yslice))
-plt.xlim(xmin, xmax)
+plt.plot(x, np.log(yslice))
+#plt.xlim(xmin, xmax)
 plt.ylim(-20,0)
 #plt.axvline(x = -res, color = 'r')
 #plt.axvline(x = res, color = 'r')
 plt.savefig('residual_central_slice.pdf')
+plt.show()
+
+#Maxrow Residual Plots
+yslice_max = res2[i]
+
+plt.plot(x, np.log(yslice_max))
+plt.ylim(-20,0)
+plt.savefig('residual_central_slice_max.pdf')
 plt.show()
 
